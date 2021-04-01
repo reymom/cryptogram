a524a802a8985820b04e5ea67af19fa8b2006792
Reymom <ramon_gs95@hotmail.com>
2021-04-01 18:16:50 +0200

    [contracts] clean functions and get artwork info

--- contracts/Artworks.sol
+++ contracts/Artworks.sol
@@ -46,6 +46,12 @@ contract Artworks is ERC721 {
         uint256 purchasePrice
     );
 
+    event artworkSupported(
+        uint256 artworkId,
+        address supporter,
+        uint likeIndex
+    );
+
     /** STRUCTS **/
     struct Artwork {
         address creator;
@@ -62,66 +68,45 @@ contract Artworks is ERC721 {
     }
 
     /*** STORAGE ***/
-    Artwork[] public artworks;
+    Artwork[] artworks;
 
     uint256 totalSupply;
 
-    mapping(uint256 => string) artworkIdtoIPFSHash;
+    mapping(address => uint) tokensCreated;
+
+    mapping(address => uint) tokensBought;
 
     mapping(string => bool) _IPFShashExists;
 
     mapping(address => uint) availableFundsForSupporter;
 
     /*** LOGIC ***/
+    function getAvailableFundsForSupporter(address _supporter) public view returns(uint) {
+        return availableFundsForSupporter[_supporter];
+    }
+
+    function withdrawSupporterFunds() public {
+        require(availableFundsForSupporter[msg.sender] > 0, 'no funds available');
+        payable(msg.sender).transfer(availableFundsForSupporter[msg.sender]);
+        availableFundsForSupporter[msg.sender] = 0;
+    }
+
+    // function withdrawLostFunds() public onlyManager {
+    //     get funds blocked in the contract which are not in availableFundsForUser
+    //     ...
+    //     payable(msg.sender).transfer(address(this).balance);
+    // }
+
     function getTotalSupply() external view returns (uint256) {
         return totalSupply;
     }
 
-    function createArtwork(
-        string memory _description,
-        string memory _tag,
-        string memory _IPFShash,
-        uint256 _initialPrice,
-        uint256 _participationPercentage
-    ) public returns (uint256) {
-        require(!_IPFShashExists[_IPFShash], 'CRG: hash already registered');
-
-        Artwork memory _artwork =
-            Artwork({
-                creator: msg.sender,
-                creationDate: block.timestamp,
-                lastPurchaseDate: 0,
-                priceSpent: 0,
-                description: _description,
-                tag: _tag,
-                IPFShash: _IPFShash,
-                initialPrice: _initialPrice,
-                participationPercentage: _participationPercentage,
-                supporters: new address[](0),
-                totalLikes: 0
-            });
-
-        artworks.push(_artwork);
-        totalSupply += 1;
-        uint256 artworkId = artworks.length - 1;
-        _mint(msg.sender, artworkId);
-        artworkIdtoIPFSHash[artworkId] = _artwork.IPFShash;
-        _IPFShashExists[_IPFShash] = true;
-
-        emit newArtwork(
-            _artwork.creator,
-            _artwork.description,
-            _artwork.tag,
-            _artwork.IPFShash,
-            _artwork.initialPrice,
-            _artwork.participationPercentage
-        );
-
-        return artworkId;
+    function getNumtokensCreated(address _creator) external view returns(uint) {
+        return tokensCreated[_creator];
     }
 
-    function getArtworkIPFSHash(uint256 _artworkId) public view returns (string memory) {
-        return artworkIdtoIPFSHash[_artworkId];
+    function getNumTokensBought(address _creator) external view returns(uint) {
+        return tokensBought[_creator];
     }
 
     function getTokensOfOwner(address _owner) external view returns (uint256[] memory) {
@@ -143,7 +128,84 @@ contract Artworks is ERC721 {
         }
     }
 
-    function supportArtwork(uint256 _artworkId) public returns (uint256) {
+    function getTokenInfo(uint _artworkId) external view returns (
+        uint, uint, uint, string memory, string memory, string memory, uint, uint, uint
+    ) {
+        Artwork memory _artwork = artworks[_artworkId];
+        return (
+            _artwork.creationDate,
+            _artwork.lastPurchaseDate,
+            _artwork.priceSpent,
+            _artwork.description,
+            _artwork.tag,
+            _artwork.IPFShash,
+            _artwork.initialPrice,
+            _artwork.participationPercentage,
+            _artwork.totalLikes
+        );
+    }
+
+    function getSupportersOfArtwork(uint256 _artworkId) external view returns(address[] memory) {
+        Artwork memory _artwork = artworks[_artworkId];
+        uint numLikes = _artwork.totalLikes;
+        if (numLikes == 0) {
+            return new address[](0);
+        } else {
+            address[] memory result = new address[](numLikes);
+            uint256 resultIndex = 0;
+            uint256 supporterId;
+            for (supporterId = 0; supporterId < numLikes; supporterId++) {
+                result[resultIndex] = _artwork.supporters[supporterId];
+                resultIndex++;
+            }
+            return result;
+        }
+    }
+
+    function createArtwork(
+        string memory _description,
+        string memory _tag,
+        string memory _IPFShash,
+        uint256 _initialPrice,
+        uint256 _participationPercentage
+    ) public returns (bool) {
+        require(!_IPFShashExists[_IPFShash], 'CRG: hash already registered');
+
+        Artwork memory _artwork =
+            Artwork({
+                creator: msg.sender,
+                creationDate: block.timestamp,
+                lastPurchaseDate: 0,
+                priceSpent: 0,
+                description: _description,
+                tag: _tag,
+                IPFShash: _IPFShash,
+                initialPrice: _initialPrice,
+                participationPercentage: _participationPercentage,
+                supporters: new address[](0),
+                totalLikes: 0
+            });
+
+        artworks.push(_artwork);
+        totalSupply = artworks.length;
+        uint256 artworkId = artworks.length - 1;
+        tokensCreated[msg.sender] += 1;
+        _mint(msg.sender, artworkId);
+        _IPFShashExists[_IPFShash] = true;
+
+        emit newArtwork(
+            _artwork.creator,
+            _artwork.description,
+            _artwork.tag,
+            _artwork.IPFShash,
+            _artwork.initialPrice,
+            _artwork.participationPercentage
+        );
+
+        return true;
+    }
+
+    function supportArtwork(uint256 _artworkId) public returns (bool) {
         // consider if make this a configurable payable parameter
         // initially, users just pay the ethereum taxes for writing in the blockchain
         // require(msg.value <= likePrice);
@@ -153,24 +215,24 @@ contract Artworks is ERC721 {
         _artwork.totalLikes += 1;
         // add supporter
         artworks[_artworkId].supporters.push(msg.sender);
-        return _artwork.totalLikes;
-    }
-
-    function getTotalLikes(uint _artworkId) external view returns(uint) {
-        Artwork memory _artwork = artworks[_artworkId];
-        return _artwork.totalLikes;
+        emit artworkSupported(
+            _artworkId,
+            msg.sender,
+            _artwork.totalLikes - 1
+        );
+        return true;
     }
 
     function _isSupporterOfArtwork(uint256 _artworkId, address _supporter) internal view returns(bool) {
         Artwork memory _artwork = artworks[_artworkId];
-        uint totalLikes = _artwork.totalLikes;
-        if (totalLikes == 0) {
+        uint numLikes = _artwork.totalLikes;
+        if (numLikes == 0) {
             return false;
         } else {
             bool isSupporter;
             uint256 resultIndex = 0;
             uint256 supporterId;
-            for (supporterId = 0; supporterId < totalLikes; supporterId++) {
+            for (supporterId = 0; supporterId < numLikes; supporterId++) {
                 if (_artwork.supporters[supporterId] == _supporter) {
                     isSupporter = true;
                 }
@@ -180,54 +242,32 @@ contract Artworks is ERC721 {
         }
     }
 
-    function getSupportersOfArtworkId(uint256 _artworkId) external view returns(address[] memory) {
-        Artwork memory _artwork = artworks[_artworkId];
-        uint totalLikes = _artwork.totalLikes;
-        if (totalLikes == 0) {
-            return new address[](0);
-        } else {
-            address[] memory result = new address[](totalLikes);
-            uint256 resultIndex = 0;
-            uint256 supporterId;
-            for (supporterId = 0; supporterId < totalLikes; supporterId++) {
-                result[resultIndex] = _artwork.supporters[supporterId];
-                resultIndex++;
-            }
-            return result;
-        }
-    }
-
-    function getCurrentPriceOfArwork(uint _artworkId) public view returns(uint) {
-        Artwork memory _artwork = artworks[_artworkId];
-        uint totalLikes = _artwork.totalLikes;
-        uint finalPrice = _artwork.initialPrice + priceIncreaseForLike * totalLikes;
-        return finalPrice;
-    }
-
     function _updateArtworkOnPurchase(uint _artworkId, uint finalPrice) internal {
         Artwork storage _artwork = artworks[_artworkId];
         _artwork.lastPurchaseDate = block.timestamp;
-        _artwork.priceSpent = finalPrice;
+        _artwork.priceSpent += finalPrice;
     }
 
     function buyArtworkToCreator(uint256 _artworkId) public payable returns (bool) {
+        require(msg.sender != ownerOf(_artworkId), 'The artwork is already yours');
         Artwork memory _artwork = artworks[_artworkId];
-        uint totalLikes = _artwork.totalLikes;
-        uint finalPrice = _artwork.initialPrice + priceIncreaseForLike * totalLikes;
+        uint numLikes = _artwork.totalLikes;
+        uint finalPrice = _artwork.initialPrice + priceIncreaseForLike * numLikes;
         require(msg.value == finalPrice, 'You must pay the price!');
         // pay the artist
         uint artistPrice = (finalPrice * (100 - _artwork.participationPercentage)) / 100;
         (bool success,) = payable(ownerOf(_artworkId)).call{value: artistPrice}('');
         require(success, 'Transfer to owner failed');
         // make the property transfer
+        tokensBought[msg.sender] += 1;
         _transfer(ownerOf(_artworkId), msg.sender, _artworkId);
         _updateArtworkOnPurchase(_artworkId, finalPrice);
 
         // register funds in order to supporters to get it later
         uint supportersPrice = (finalPrice * _artwork.participationPercentage) / 100;
         uint supporterId;
-        uint supporterPrice = supportersPrice / _artwork.totalLikes;
-        for (supporterId = 0; supporterId < _artwork.totalLikes; supporterId++) {
+        uint supporterPrice = supportersPrice / numLikes;
+        for (supporterId = 0; supporterId < numLikes; supporterId++) {
             // address payable supporter = payable(_artwork.supporters[supporterId]);
             // (bool succeses,) = supporter.call{value: supporterPrice}('');
             // require(succeses, 'Transfer to supporters failed');
@@ -236,29 +276,13 @@ contract Artworks is ERC721 {
         return true;
     }
 
-    function buyArtworkToCollectionist(uint256 _artworkId) public payable returns (bool) {
-        // lo mismo pero un TransferFrom, así que necesitará aceptación antes
-        // en buy to creator, quizas resetear los supporters para que solo los nuevos recivan
-        // en ningun caso el num de likes
-        uint tmp = _artworkId + 1;
-        require(tmp != 0);
-        return true;
-    }
-
-    function getAvailableFundsForSupporter(address _supporter) public view returns(uint) {
-        return availableFundsForSupporter[_supporter];
-    }
-
-    function withdrawSupporterFunds() public {
-        require(availableFundsForSupporter[msg.sender] > 0, 'no funds available');
-        payable(msg.sender).transfer(availableFundsForSupporter[msg.sender]);
-        availableFundsForSupporter[msg.sender] = 0;
-    }
-
-    // function withdrawLostFunds() public onlyManager {
-    //     get funds blocked in the contract which are not in availableFundsForUser
-    //     ...
-    //     payable(msg.sender).transfer(address(this).balance);
+    // function buyArtworkToCollectionist(uint256 _artworkId) public payable returns (bool) {
+    //     // lo mismo pero un TransferFrom, así que necesitará aceptación antes
+    //     // en buy to creator, quizas resetear los supporters para que solo los nuevos recivan
+    //     // en ningun caso el num de likes
+    //     uint tmp = _artworkId + 1;
+    //     require(tmp != 0);
+    //     return true;
     // }
 
 }
--- test/Artworks.test.js
+++ test/Artworks.test.js
@@ -16,7 +16,7 @@ contract('Cryptogram', (accounts) => {
         // const decaymentUnit = (await contract.decaymentUnit()).toNumber();
     });
 
-    describe('deployment', async() => {
+    describe('contract deployment', async() => {
 
         it('deploys successfully', async() => {
             const address = contract.address;
@@ -36,7 +36,7 @@ contract('Cryptogram', (accounts) => {
 
     });
 
-    describe('create artwork', async() => {
+    describe('artwork creation', async() => {
 
         it('creates a new artwork', async() => {
             const result = await contract.createArtwork(
@@ -44,7 +44,7 @@ contract('Cryptogram', (accounts) => {
                 'testTag',
                 'Qme9fWvWYjK4FLcmcDVM1wPKvxcwzqJKXhoBs4ZMEmbrFa',
                 web3.utils.toWei('1', 'ether'),
-                50
+                50 // participation percentage
             );
             // SUCCESS
             // balance
@@ -62,15 +62,12 @@ contract('Cryptogram', (accounts) => {
                 'anotherTestTag',
                 'Qme9fWvWYjK4FLcmcDVM1wPKvxcwzqJKXhoBs4ZMEmbrFa',
                 web3.utils.toWei('0.5', 'ether'),
-                10 // participation percentage
+                10
             ).should.be.rejected;
         });
-    });
 
-    describe('artworks indexing', async() => {
-
-        it('lists hashes', async() => {
-            // Mint 3 tokens
+        it('creates multiple artworks and lists hashes', async() => {
+            // Mint 3 more tokens
             let hashes = [
                 'Qme9fWvWYjK4FLcmcDVM1wPKvxcwzqJKXhoBs4ZMEmbrFa', // the one already registered
                 'JNS8fsknsfJKF33OfsdnI4Nfo4F9F4nfsOFNfFLNDCSDb9',
@@ -79,78 +76,92 @@ contract('Cryptogram', (accounts) => {
             ];
             for (var i = 1; i <= 3; i++) {
                 await contract.createArtwork(
-                    'Artwork' + i.toString(), 'tag' + i.toString(), hashes[i], web3.utils.toWei('0.1', 'ether'), 50
+                    'Artwork' + i.toString(), 'tag' + i.toString(), 
+                    hashes[i], 
+                    web3.utils.toWei('0.1', 'ether'), 
+                    10, { from: accounts[i] }
                 );
             }
-            const balanceOf = await contract.balanceOf(accounts[0]);
-            // Test whether it has been correctly indexed
-            let hash;
+            // Test whether hash has been correctly registered
+            let tokenInfo;
             let result = [];
-            for (var i = 0; i < balanceOf; i++) {
-                hash = await contract.getArtworkIPFSHash(i);
-                result.push(hash);
+            for (var i = 0; i < hashes.length; i++) {
+                tokenInfo = await contract.getTokenInfo(i);
+                result.push(tokenInfo[5]);
             }
-            assert.equal(result.join(','), hashes.join(','));
+            assert.equal(result.join(','), hashes.join(','), 'correct hashes');
         });
+
+        it('indexes artworks', async() => {
+            let tokenOwners = [];
+            let tokenList;
+            for (var i = 0; i <= 3; i++) {
+                tokenList = await contract.getTokensOfOwner(accounts[i]);
+                tokenOwners.push(tokenList[0].toNumber());
+            }
+            assert.equal([0, 1, 2, 3].join(','), tokenOwners.join(','));
+        });
+
     });
 
-    describe('support', async() => {
+    describe('artwork support', async() => {
+        const artworkId = 0;
 
-        it('adds and indexes likes', async() => {
-            const artworkId = 0;
+        it('allows one support per artwork', async() => {
             // creator cannot make like
             await contract.supportArtwork(artworkId, {from: accounts[0]}).should.be.rejected;
             // cannot like twice
-            await contract.supportArtwork(artworkId, {from: accounts[1]});
+            let result = await contract.supportArtwork(artworkId, {from: accounts[1]});
             await contract.supportArtwork(artworkId, {from: accounts[1]}).should.be.rejected;
-            // 6 likes in total
+        });
+
+        it('indexes and adds supporters', async() => {
             const desiredLikes = 6;
             for (var i = 2; i <= desiredLikes; i++) {
                 await contract.supportArtwork(artworkId, {from: accounts[i]});
             }
-            // check the stored data in the contract
-            const totalLikes = await contract.getTotalLikes(artworkId);
+            const totalLikes = (await contract.getTokenInfo(artworkId))[8];
             assert.equal(totalLikes.toNumber(), desiredLikes, 'correct number of likes');
-            const supporters = await contract.getSupportersOfArtworkId(artworkId);
+            const supporters = await contract.getSupportersOfArtwork(artworkId);
             assert.equal(
                 supporters.join(','),
                 accounts.slice(1, desiredLikes + 1).join(','),
                 'correct supporters'
             );
-        });
+        })
 
     });
 
-    describe('artworks purchase', async() => {
+    describe('artwork purchase', async() => {
 
-        it('calculate prices and pays the artist', async() => {
-            // account 7 will buy the artwork of account 0 supported by 1, 2, 3, 4, 5 and 6
+        it('pays the artist', async() => {
+            // account 9 will buy the artwork of account 0 supported by 1, 2, 3, 4, 5 and 6
             const artworkId = 0;
             initialArtistBalance = await web3.eth.getBalance(accounts[0]);
-            // calculate artwork price
             const weiAmount = web3.utils.toWei('1.6', 'ether');
-            const artworkPrice = await contract.getCurrentPriceOfArwork(artworkId);
-            assert.equal(artworkPrice, weiAmount, 'Price correctly set');
             // buy and transfer to artist
-            await contract.buyArtworkToCreator(artworkId, { from: accounts[7], value: weiAmount });
+            await contract.buyArtworkToCreator(artworkId, { from: accounts[9], value: weiAmount });
             const creatorGain = await web3.eth.getBalance(accounts[0]) - initialArtistBalance;
             assert.equal(creatorGain, weiAmount / 2, 'correct artist payment amount');
         });
 
         it('transfers artwork property', async() => {
             newOwner = await contract.ownerOf(0);
-            assert.equal(newOwner, accounts[7], 'correct new owner');
+            assert.equal(newOwner, accounts[9], 'correct new owner');
         });
 
-        it('allows supporters to extract funds', async() => {
+    });
+
+    describe('supporter funds managing', async() => {
+
+        it('register funds and allows extraction', async() => {
             let initialBalances = [];
             for (var i = 1; i <= 6; i++) {
                 initialBalances.push(await web3.eth.getBalance(accounts[i]));
             }
-            // must handle precision, as we have not decimals
             // TODO: handle this in the contract
-            //      with safemath
-            //      and maybe transfering up to a decimal precition and see the rest as admin comission :)
+            //       with safemath
+            //       and maybe transfering up to a decimal precition and see the rest as admin comission :)
             weiDecimalPrecision = 2;
             const weiForSupporters = web3.utils.toWei('0.8', 'ether');
             const weiForSupporter = (weiForSupporters / 6).toString().slice(0, -weiDecimalPrecision);
@@ -168,7 +179,6 @@ contract('Cryptogram', (accounts) => {
             await contract.withdrawSupporterFunds({from: accounts[1]}).should.be.rejected;
             assert.equal(leftFunds, 0, 'funds extracted correctly');
         });
-
     });
 
 })
